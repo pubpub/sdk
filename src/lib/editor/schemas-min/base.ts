29da3a385b0d5eb25b0d5cb07df56eeb27c16097
Thomas F. K. Jorna <hello@tefkah.com>
2023-05-03 16:01:41 +0200

    fix: add dom parsing capabilities

--- src/lib/editor/schemas-min/base.ts
+++ src/lib/editor/schemas-min/base.ts
@@ -1,4 +1,4 @@
-import { DOMOutputSpec, NodeSpec } from 'prosemirror-model'
+import { DOMOutputSpec, MarkSpec, NodeSpec } from 'prosemirror-model'
 
 export const baseNodes: { [key: string]: NodeSpec } = {
   doc: {
@@ -18,6 +18,36 @@ export const baseNodes: { [key: string]: NodeSpec } = {
       textAlign: { default: null },
       rtl: { default: null },
     },
+    parseDOM: [
+      {
+        tag: 'p',
+        getAttrs: (node) => {
+          return {
+            id: (node as Element).getAttribute('id'),
+            class: (node as Element).getAttribute('class'),
+            textAlign: (node as Element).getAttribute('data-text-align'),
+            rtl: (node as Element).getAttribute('data-rtl'),
+          }
+        },
+      },
+    ],
+    toDOM: (node) => {
+      const isEmpty =
+        !node.content || (Array.isArray(node.content) && !node.content.length)
+      const children = isEmpty ? ['br'] : 0
+      return [
+        'p',
+        {
+          class: node.attrs.class,
+          ...(node.attrs.id && { id: node.attrs.id }),
+          ...(node.attrs.textAlign && {
+            'data-text-align': node.attrs.textAlign,
+          }),
+          ...(node.attrs.rtl && { 'data-rtl': node.attrs.rtl.toString() }),
+        },
+        children,
+      ] as DOMOutputSpec
+    },
   },
   blockquote: {
     content: 'block+',
@@ -26,11 +56,31 @@ export const baseNodes: { [key: string]: NodeSpec } = {
       id: { default: null },
     },
     selectable: false,
+    parseDOM: [
+      {
+        tag: 'blockquote',
+        getAttrs: (node) => {
+          return {
+            id: (node as Element).getAttribute('id'),
+          }
+        },
+      },
+    ],
+    toDOM: (node) => {
+      return [
+        'blockquote',
+        { ...(node.attrs.id && { id: node.attrs.id }) },
+        0,
+      ] as DOMOutputSpec
+    },
   },
   horizontal_rule: {
     group: 'block',
     parseDOM: [{ tag: 'hr' }],
     selectable: true,
+    toDOM: () => {
+      return ['div', ['hr']] as DOMOutputSpec
+    },
   },
   heading: {
     attrs: {
@@ -44,6 +94,32 @@ export const baseNodes: { [key: string]: NodeSpec } = {
     group: 'block',
     defining: true,
     selectable: false,
+    parseDOM: [1, 2, 3, 4, 5, 6].map((level) => {
+      return {
+        tag: `h${level}`,
+        getAttrs: (node) => {
+          return {
+            id: (node as Element).getAttribute('id'),
+            textAlign: (node as Element).getAttribute('data-text-align'),
+            rtl: (node as Element).getAttribute('data-rtl'),
+            level,
+          }
+        },
+      }
+    }),
+    toDOM: (node) => {
+      return [
+        `h${node.attrs.level}`,
+        {
+          id: node.attrs.fixedId || node.attrs.id,
+          ...(node.attrs.textAlign && {
+            'data-text-align': node.attrs.textAlign,
+          }),
+          ...(node.attrs.rtl && { 'data-rtl': node.attrs.rtl.toString() }),
+        },
+        0,
+      ] as DOMOutputSpec
+    },
   },
   ordered_list: {
     content: 'list_item+',
@@ -54,6 +130,34 @@ export const baseNodes: { [key: string]: NodeSpec } = {
       rtl: { default: null },
     },
     selectable: false,
+    parseDOM: [
+      {
+        tag: 'ol',
+        getAttrs: (node) => {
+          return {
+            id: (node as Element).getAttribute('id'),
+            order: (node as Element).hasAttribute('start')
+              ? +(node as Element).getAttribute('start')!
+              : 1,
+            rtl: (node as Element).getAttribute('data-rtl'),
+          }
+        },
+      },
+    ],
+    toDOM: (node) => {
+      return [
+        'ol',
+        {
+          ...(node.attrs.id && { id: node.attrs.id }),
+          ...(node.attrs.textAlign && {
+            'data-text-align': node.attrs.textAlign,
+          }),
+          ...(node.attrs.rtl && { 'data-rtl': node.attrs.rtl.toString() }),
+          start: node.attrs.order === 1 ? null : node.attrs.order,
+        },
+        0,
+      ] as DOMOutputSpec
+    },
   },
   bullet_list: {
     content: 'list_item+',
@@ -63,27 +167,94 @@ export const baseNodes: { [key: string]: NodeSpec } = {
       rtl: { default: null },
     },
     selectable: false,
+    parseDOM: [
+      {
+        tag: 'ul',
+        getAttrs: (node) => {
+          return {
+            id: (node as Element).getAttribute('id'),
+            rtl: (node as Element).getAttribute('data-rtl'),
+          }
+        },
+      },
+    ],
+    toDOM: (node) => {
+      return [
+        'ul',
+        {
+          ...(node.attrs.id && { id: node.attrs.id }),
+          ...(node.attrs.textAlign && {
+            'data-text-align': node.attrs.textAlign,
+          }),
+          ...(node.attrs.rtl && { 'data-rtl': node.attrs.rtl.toString() }),
+        },
+        0,
+      ] as DOMOutputSpec
+    },
   },
   list_item: {
     content: 'paragraph block*',
     defining: true,
     selectable: false,
+    parseDOM: [{ tag: 'li' }],
+    toDOM: () => {
+      return ['li', 0] as DOMOutputSpec
+    },
   },
   text: {
     inline: true,
     group: 'inline',
+    toDOM: (node) => {
+      return node.text!
+    },
   },
   hard_break: {
     inline: true,
     group: 'inline',
     selectable: false,
+    parseDOM: [{ tag: 'br' }],
+    toDOM: () => {
+      return ['br'] as DOMOutputSpec
+    },
   },
 }
 
-export const baseMarks = {
-  em: {},
+export const baseMarks: Record<string, MarkSpec> = {
+  em: {
+    parseDOM: [
+      { tag: 'i' },
+      { tag: 'em' },
+      {
+        style: 'font-style',
+        getAttrs: (value) => value === 'italic' && null,
+      },
+    ],
+    toDOM: () => {
+      return ['em'] as DOMOutputSpec
+    },
+  },
 
-  strong: {},
+  strong: {
+    parseDOM: [
+      { tag: 'strong' },
+      /*
+			This works around a Google Docs misbehavior where
+			pasted content will be inexplicably wrapped in `<b>`
+			tags with a font-weight normal.
+			*/
+      {
+        tag: 'b',
+        getAttrs: (node) => node.style.fontWeight !== 'normal' && null,
+      },
+      {
+        style: 'font-weight',
+        getAttrs: (value) => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null,
+      },
+    ],
+    toDOM: () => {
+      return ['strong'] as DOMOutputSpec
+    },
+  },
   link: {
     inclusive: false,
     attrs: {
@@ -91,9 +262,54 @@ export const baseMarks = {
       title: { default: null },
       target: { default: null },
     },
+    parseDOM: [
+      {
+        tag: 'a[href]',
+        getAttrs: (dom) => {
+          if (dom.getAttribute('data-node-type') === 'reference') {
+            return false
+          }
+          return {
+            href: dom.getAttribute('href'),
+            title: dom.getAttribute('title'),
+            target: dom.getAttribute('target'),
+          }
+        },
+      },
+    ],
+    toDOM: (node) => {
+      /* Links seem to be recieving a target attr that is a dom element */
+      /* coming from the wrong source in some interfaces. This ensures */
+      /* only strings can be a target attr. */
+      const attrs = node.attrs
+      if (attrs.target && typeof attrs.target !== 'string') {
+        attrs.target = null
+      }
+      return ['a', attrs] as DOMOutputSpec
+    },
   },
-  sub: {},
-  sup: {},
-  strike: {},
-  code: {},
+  sub: {
+    parseDOM: [{ tag: 'sub' }],
+    toDOM: () => {
+      return ['sub'] as DOMOutputSpec
+    },
+  },
+  sup: {
+    parseDOM: [{ tag: 'sup' }],
+    toDOM: () => {
+      return ['sup'] as DOMOutputSpec
+    },
+  },
+  strike: {
+    parseDOM: [{ tag: 's' }, { tag: 'strike' }, { tag: 'del' }],
+    toDOM: () => {
+      return ['s'] as DOMOutputSpec
+    },
+  },
+  code: {
+    parseDOM: [{ tag: 'code' }],
+    toDOM: () => {
+      return ['code'] as DOMOutputSpec
+    },
+  },
 }
