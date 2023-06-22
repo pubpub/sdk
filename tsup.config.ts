3175ff4e0270f51fd9b93772fe710bf3db3ea588
Thomas F. K. Jorna <hello@tefkah.com>
2023-06-22 12:56:33 +0200

    fix: add moduleResolution: node16 support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'tsup'
+import { copyFile } from 'fs/promises'
+import readdirp from 'readdirp'
 
 const config = defineConfig({
   format: ['cjs', 'esm'],
@@ -6,6 +8,26 @@ const config = defineConfig({
   clean: true,
   bundle: false,
   entry: ['src'],
+  onSuccess: async () => {
+    const dir = await readdirp.promise(
+      new URL('./dist', import.meta.url).pathname
+    )
+    // copy each .d.ts file to a .d.cts file
+    await Promise.all(
+      dir
+
+        .filter((file) => file.path.endsWith('.d.ts'))
+        .map(async (file) => {
+          await copyFile(
+            new URL(`./dist/${file.path}`, import.meta.url).pathname,
+            new URL(
+              `./dist/${file.path.replace('.ts', '')}.cts`,
+              import.meta.url
+            ).pathname
+          )
+        })
+    )
+  },
 })
 
 export default config
