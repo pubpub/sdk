78e386d029266bb1d5f84087647270235ef650bb
Thomas F. K. Jorna <hello@tefkah.com>
2023-09-11 14:27:39 +0200

    feat: make build and tests work again

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  test: {
+    dir: 'test',
+  },
   plugins: [
     viteTsConfigPaths({
-      root: './..',
+      root: '.',
     }),
   ],
 })
